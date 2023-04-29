f1cf2decd61246d6c55b45fecf2776b12d3f6935
zak <60068872+Zakovskiy@users.noreply.github.com>
2023-04-29 19:49:21 +0300

    .

--- app/src/main/java/com/Zakovskiy/lwaf/room/RoomActivity.java
+++ app/src/main/java/com/Zakovskiy/lwaf/room/RoomActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import com.Zakovskiy.lwaf.ABCActivity;
+import com.Zakovskiy.lwaf.DialogTextBox;
 import com.Zakovskiy.lwaf.R;
 import com.Zakovskiy.lwaf.dashboard.adapters.RoomsAdapter;
+import com.Zakovskiy.lwaf.globalConversation.GlobalConversationActivity;
 import com.Zakovskiy.lwaf.models.RoomInLobby;
 import com.Zakovskiy.lwaf.network.SocketHelper;
+import com.Zakovskiy.lwaf.utils.Config;
+import com.Zakovskiy.lwaf.utils.PacketDataKeys;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class RoomActivity extends ABCActivity implements SocketHelper.SocketListener {
@@ -23,6 +27,18 @@ public class RoomActivity extends ABCActivity implements SocketHelper.SocketList
         this.room = (RoomInLobby) getIntent().getSerializableExtra("room");
         /* V razrabotke */
     }
+
+    @Override
+    public void onReceive(JsonNode json) {
+        this.runOnUiThread(() -> {
+            if (json.has(PacketDataKeys.ERROR)) {
+                new DialogTextBox(RoomActivity.this, Config.ERRORS.get(json.get(PacketDataKeys.ERROR).asInt())).show();
+            } else if (json.has(PacketDataKeys.TYPE_EVENT)) {
+                String typeEvent = json.get(PacketDataKeys.TYPE_EVENT).asText();
+            }
+        });
+    }
+
     @Override
     public void onConnected() {
 
@@ -30,11 +46,6 @@ public class RoomActivity extends ABCActivity implements SocketHelper.SocketList
 
     @Override
     public void onDisconnected() {
-
-    }
-
-    @Override
-    public void onReceive(JsonNode json) {
 
     }
 
