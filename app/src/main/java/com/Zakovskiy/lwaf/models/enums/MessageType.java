e6491afcc15ff59ea5139c21c57a999e7234c96f
zak <60068872+Zakovskiy@users.noreply.github.com>
2023-05-01 03:59:19 +0300

    add new messagetypes

--- app/src/main/java/com/Zakovskiy/lwaf/models/enums/MessageType.java
+++ app/src/main/java/com/Zakovskiy/lwaf/models/enums/MessageType.java
@@ -4,7 +4,14 @@ public enum MessageType {
     UNDEFINED(0),
     TEXT(1),
     JOIN(2),
-    LEFT(3);
+    LEFT(3),
+    ADD_TRACK(4),
+    SET_REACTION_LIKE(5),
+    SET_REACTION_DISLIKE(6),
+    SET_REACTION_SUPER_LIKE(7),
+    LOTO_WINNERS(8),
+    REPLACE_TRACK(9),
+    DELETE_TRACK(10);
 
     private Integer type;
 
